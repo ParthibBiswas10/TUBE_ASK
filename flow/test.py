1e69aeba1c243b884c5a623b51885345b8f3df65
ParthibBiswas10 <parthiblm10@gmail.com>
2026-02-23 01:15:18 +0530

    added Embedding

--- flow/test.py
+++ flow/test.py
@@ -1,6 +1,10 @@
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
+from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain_huggingface import HuggingFaceEndpointEmbeddings
 import os
 
 load_dotenv()
@@ -8,5 +12,14 @@ llm = ChatGroq(
     model="llama-3.1-8b-instant", 
     api_key=os.getenv("GROQ_API_KEY")
 )
-rsult=llm.invoke("Todays Date?")
+
+embeddings = HuggingFaceEndpointEmbeddings(
+    model="sentence-transformers/all-MiniLM-L6-v2",
+    huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_API_TOKEN")
+)
+
+rsult=llm.invoke("WHO IS MESSI?")
 print(rsult.content)
+
+result_embedding = embeddings.embed_query(rsult.content)
+print(result_embedding)
